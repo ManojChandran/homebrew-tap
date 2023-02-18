213b19096094a080293e974fa8718dfbf80c305d
Manoj Chandran <manojchandran@Manojs-MBP.bbrouter>
2023-02-18 23:38:01 +0530

    update odebo

--- odebo.rb
+++ odebo.rb
@@ -2,14 +2,11 @@ class Odebo < Formula
   desc "Simple SONARQUBE CLI"
   homepage ""
   url "https://github.com/ManojChandran/odebo/releases/download/v0.1.0/odebo_Darwin_x86_64.tar.gz"
-  sha256 "1c8dd5bde63a05ac67c0b518216a498e40a3df02"
+  sha256 "a275d395281566dca61e194a2ff2108503b96d84a6159e82c71916e4182a9d68"
   license "Apache License 2.0"
 
   def install
     bin install "odebo" 
   end
 
-  test do
-    system "#{bin}/odebo"
-  end
 end
