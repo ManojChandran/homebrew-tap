7c1c6f38d65f17814b3ef345f66f3dd16350c82f
Manoj Chandran <manojchandran@Manojs-MBP.bbrouter>
2023-03-15 03:17:28 +0530

    update arch

--- odebo_darwin_arm64.rb
+++ odebo_darwin_arm64.rb
@@ -1,7 +1,7 @@
 class OdeboDarwinArm64 < Formula
   desc "Simple Sonarqube CLI"
   homepage "https://github.com/Manojchandran/odebo"
-  sha256 "19c6da3cbb6bbab9d4923d33120d7468e628d295096c23f579f4e0a6179718a9"
+  sha256 ""
   license "Apache License 2.0"
 
   def install
